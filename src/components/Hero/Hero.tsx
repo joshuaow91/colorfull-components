11573717affff5e52458c19d23e73b7f355510bb
Joshua Ow <joshuaow@gmail.com>
2024-09-17 11:14:18 -0500

    added link to logo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="flex  items-center justify-center ">
       <AnimatedGradientText className="text-8xl tracking-tight">
-        Colorfull
+        <a href="/">Colorfull</a>
       </AnimatedGradientText>
     </div>
   );
