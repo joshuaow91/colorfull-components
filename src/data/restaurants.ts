d276fa42fd2cac956e6a54a0d684f74b31e42cb3
Joshua Ow <joshuaow@gmail.com>
2024-09-17 20:15:09 -0500

    debugging netlify image issue

--- src/data/restaurants.ts
+++ src/data/restaurants.ts
@@ -4,31 +4,31 @@ export const restaurants: Restaurant[] = [
   {
     id: 1,
     name: "Pizza Place",
-    image: "/pizza2.png",
+    image: "/images/pizza2.png",
   },
   {
     id: 2,
     name: "Sushi House",
-    image: "/sushi2.png",
+    image: "/images/sushi2.png",
   },
   {
     id: 3,
     name: "Burger Joint",
-    image: "/burger2.png",
+    image: "/images/burger2.png",
   },
   {
     id: 4,
     name: "Taco Spot",
-    image: "/taco2.png",
+    image: "/images/taco2.png",
   },
   {
     id: 5,
     name: "Pasta Corner",
-    image: "/pasta2.png",
+    image: "/images/pasta2.png",
   },
   {
     id: 6,
     name: "Salad Bar",
-    image: "/salad2.png",
+    image: "/images/salad2.png",
   },
 ];
