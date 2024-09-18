3c939617db08f11337f5515519f4161be448fc56
Joshua Ow <joshuaow@gmail.com>
2024-09-17 19:20:24 -0500

    imported images directly, fix broken images in deployment

--- src/data/restaurants.ts
+++ src/data/restaurants.ts
@@ -1,34 +1,41 @@
 import { Restaurant } from "../types/restaurantTypes";
 
+import pizzaImage from "../../public/pizza2.png";
+import sushiImage from "../../public/sushi2.png";
+import burgerImage from "../../public/burger2.png";
+import tacoImage from "../../public/taco2.png";
+import pastaImage from "../../public/pasta2.png";
+import saladImage from "../../public/salad2.png";
+
 export const restaurants: Restaurant[] = [
   {
     id: 1,
     name: "Pizza Place",
-    image: "/pizza2.png",
+    image: pizzaImage,
   },
   {
     id: 2,
     name: "Sushi House",
-    image: "/sushi2.png",
+    image: sushiImage,
   },
   {
     id: 3,
     name: "Burger Joint",
-    image: "/burger2.png",
+    image: burgerImage,
   },
   {
     id: 4,
     name: "Taco Spot",
-    image: "/taco2.png",
+    image: tacoImage,
   },
   {
     id: 5,
     name: "Pasta Corner",
-    image: "/pasta2.png",
+    image: pastaImage,
   },
   {
     id: 6,
     name: "Salad Bar",
-    image: "/salad2.png",
+    image: saladImage,
   },
 ];
