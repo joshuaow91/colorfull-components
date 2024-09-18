603fceaf37b0d7d473ca65f1bbb296d6fd923821
Joshua Ow <joshuaow@gmail.com>
2024-09-18 10:17:46 -0500

    added space for top of the logo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import AnimatedGradientText from "./AnimatedGradientText";
 
 export default function Hero() {
   return (
-    <div className="flex  items-center justify-center ">
+    <div className="flex items-center justify-center pt-8">
       <AnimatedGradientText className="text-8xl tracking-tight">
         <a href="/">Colorfull</a>
       </AnimatedGradientText>
